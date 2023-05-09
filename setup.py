69c8905c5ac3511679a728d1daa97e9a60a97f99
Liliana Vargas <liliana.meleza@gmail.com>
2023-05-09 20:59:21 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='sualization of the main geothermal targets in NL',
+    author='lvm',
+    license='MIT',
+)
